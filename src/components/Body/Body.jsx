d672195b503d55a2f036dea2a69ba5c0da9a6f90
Shahi77 <shahiinfinity02@gmail.com>
2024-02-05 18:05:22 +0530

    adding routes to login and browse pages

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Login from "../../pages/Login/Login";
 import Browse from "../../pages/Browse/Browse";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/Browse",
+      element: <Browse />,
+    },
+  ]);
   return (
     <div>
-      <Login />
-      <Browse />
+      <RouterProvider router={appRouter} />
     </div>
   );
 };
