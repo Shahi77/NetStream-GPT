de670d47075943ac0e864ec91341cc8ed6b3ee17
Shahi77 <shahiinfinity02@gmail.com>
2024-02-05 17:54:30 +0530

    adding basic layout structure to my app

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../../pages/Login/Login";
+import Browse from "../../pages/Browse/Browse";
+
+const Body = () => {
+  return (
+    <div>
+      <Login />
+      <Browse />
+    </div>
+  );
+};
+
+export default Body;
